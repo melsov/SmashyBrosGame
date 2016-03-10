02d81213b12a514f6a965bee271fa5a1830a0577
Mel Sovereign <melsovereign@yahoo.com>
2016-03-10 08:24:15 -0500

    Punching initiates check for getting punched

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.Assertions;
 using System.Collections;
+using System;
 
 public class PlayerControls : MonoBehaviour {
 
     protected Rigidbody rb;
     protected Transform fist;
+    protected Collider fistMesh;
     protected bool grounded;
     public float moveSpeed = 2f;
     public float maxMoveSpeed = 3f;
@@ -16,6 +18,7 @@ public class PlayerControls : MonoBehaviour {
     protected string horizontal;
     protected KeyCode jumpKey;
     protected KeyCode punchKey;
+    protected PlayerControls otherGuysControls;
 
     public float punchTimeSeconds = .3f;
     protected float punchTimer;
@@ -25,12 +28,18 @@ public class PlayerControls : MonoBehaviour {
     public float sensitivityIncrement = 5f;
     private float reactToAPunchTime;
 
+    private Collider _body;
+    public Collider body {
+        get { return _body; }
+    }
+
     void Awake () {
         rb = GetComponentInChildren<Rigidbody>();
 
         foreach(Transform t in GetComponentsInChildren<Transform>()) {
             if (t.CompareTag("Fist")) {
                 fist = t;
+                fistMesh = fist.GetComponentInChildren<Collider>();
                 break;
             }
         }
@@ -41,11 +50,22 @@ public class PlayerControls : MonoBehaviour {
             horizontal = "Horizontal";
             jumpKey = KeyCode.W;
             punchKey = KeyCode.E;
+            otherGuysControls = GameObject.Find("PlayerTwo").GetComponent<PlayerControls>();
         } else {
             horizontal = "Horizontal2";
             jumpKey = KeyCode.I;
             punchKey = KeyCode.O;
+            otherGuysControls = GameObject.Find("PlayerOne").GetComponent<PlayerControls>();
         }
+        Assert.IsTrue(otherGuysControls != null, "what???! didn't find the other guy");
+
+        foreach(Collider c in GetComponentsInChildren<Collider>()) {
+            if (c.CompareTag("Body")) {
+                _body = c;
+                break;
+            }
+        }
+        Assert.IsTrue(_body != null, "huh?? didn't find body collider.");
 	}
 	
 	void Update () {
@@ -62,9 +82,7 @@ public class PlayerControls : MonoBehaviour {
 
         if (Mathf.Abs(Input.GetAxis(horizontal)) > Mathf.Epsilon) {
             float yAxisRotation = 90f - ( Mathf.Sign(Input.GetAxis(horizontal)) * 90f);
-            print(yAxisRotation);
             rb.MoveRotation(Quaternion.Euler(0f, yAxisRotation, 0f));
-            //transform.rotation = Quaternion.Euler(0f, yAxisRotation, 0f);
         }
 
 
@@ -86,6 +104,13 @@ public class PlayerControls : MonoBehaviour {
     protected void punch() {
         fist.gameObject.SetActive(true);
         punchTimer = Time.fixedTime;
+        checkPunchConnected();
+    }
+
+    protected void checkPunchConnected() {
+        if (fistMesh.bounds.Intersects(otherGuysControls.body.bounds)) {
+            otherGuysControls.getPunched(fist.transform);
+        }
     }
 
     void OnCollisionEnter(Collision collision) {
@@ -96,11 +121,18 @@ public class PlayerControls : MonoBehaviour {
     }
 
     void OnTriggerEnter(Collider other) {
-        if(other.gameObject.CompareTag("FistMesh")) {
-            if (other.transform.parent.gameObject != fist) {
-                getPunched(other.transform);
-            } 
+        //if(other.gameObject.CompareTag("FistMesh")) {
+        //    if (other.transform.parent.gameObject != fist) {
+        //        //getPunched(other.transform);
+        //    } 
+        //}
+        if (other.gameObject.CompareTag("DeathArea")) {
+            loseRound();
         }
+    }
+
+    private void loseRound() {
+        throw new NotImplementedException();
     }
 
     protected void getPunched(Transform fist) {
